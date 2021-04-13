15195893e7015e6352c3c3d41e8ad6573ad0dc1e
Laffire <phongvan4422@gmail.com>
2021-04-13 09:07:47 +0700

    Add Import Excel File For Lecturer

--- DACN/AddLecturerExcelFile.cs
+++ DACN/AddLecturerExcelFile.cs
@@ -30,17 +30,79 @@ namespace DACN
         Connection connection = new Connection();
         private void btnImport_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                DapperPlusManager.Entity<ImportLecturerExcelFile>().Table("Lecturer");
+                List<ImportLecturerExcelFile> importLecturerExcelFiles = lecturerBindingSource1.DataSource as List<ImportLecturerExcelFile>;
+                if (importLecturerExcelFiles != null)
+                {
+                    using (IDbConnection db = connection.GetSqlConnection())
+                    {
+                        db.BulkInsert(importLecturerExcelFiles);
+                    }
+                }
+                MessageBox.Show("All information have been imported", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
 
         private void btnBrowse_Click(object sender, EventArgs e)
         {
-            
+            using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "Excel Sheet (*.xls)|*.xls|WPS Excel (*xlsx)|*.xlsx|All Files(*.*)|*.*" })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = openFileDialog.FileName;
+                    using (var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                            });
+                            tableCollection = result.Tables;
+                            cboSheet.Items.Clear();
+                            foreach (DataTable table in tableCollection)
+                                cboSheet.Items.Add(table.TableName);
+                        }
+                    }
+                }
+            }
+
         }
         DataTableCollection tableCollection;
         private void cboSheet_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            try
+            {
+                DataTable dt = tableCollection[cboSheet.SelectedItem.ToString()];
+                if (dt != null)
+                {
+                    List<ImportLecturerExcelFile> imports = new List<ImportLecturerExcelFile>();
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        ImportLecturerExcelFile import = new ImportLecturerExcelFile();
+                        import.FirstName = dt.Rows[i]["FirstName"].ToString();
+                        import.MidName = dt.Rows[i]["MidName"].ToString();
+                        import.LastName = dt.Rows[i]["LastName"].ToString();
+                        import.Email = dt.Rows[i]["Email"].ToString();
+                        import.Phone = dt.Rows[i]["Phone"].ToString();
+                        import.Birth = dt.Rows[i]["Birth"].ToString();
+                        import.Status = dt.Rows[i]["Status"].ToString();
+                        imports.Add(import);
+                    }
+                    lecturerBindingSource1.DataSource = imports;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
