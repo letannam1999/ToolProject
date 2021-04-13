80a4acdd7fe55f1934f291ecfbc909c28567d736
HoangThienQuang <quang2.11.1999@gmail.com>
2021-04-13 11:11:55 +0700

    Add One Subject

--- DACN/AddOneSubject.cs
+++ DACN/AddOneSubject.cs
@@ -20,12 +20,43 @@ namespace DACN
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                if (txtSubjectId.Text.Trim().Equals("") || txtPeriods.Text.Trim().Equals("") || txtName.Text.Trim().Equals("") || txtCredits.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("Required Feilds - Missing Information", "Required Feilds", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    String subjectId = txtSubjectId.Text;
+                    String name = txtName.Text;
+                    String periods = txtPeriods.Text;
+                    String credits = txtCredits.Text;
+
+                    Boolean insertSubject = subject.insertSubject(subjectId, name, periods, credits);
+
+                    if (insertSubject)
+                    {
+                        MessageBox.Show("New Subject Has Been Added", "Add Subject", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error - Can Not Add New Subject", "Add Subject", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-           
+            txtCredits.Text = "";
+            txtName.Text = "";
+            txtPeriods.Text = "";
+            txtSubjectId.Text = "";
         }
     }
 }
