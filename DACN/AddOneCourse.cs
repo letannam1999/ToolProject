80270198e26daf9919b8738bce98ab88b69d1b2e
letannam1999 <namlee151199@gmail.com>
2021-04-13 11:04:49 +0700

    Add One Course

--- DACN/AddOneCourse.cs
+++ DACN/AddOneCourse.cs
@@ -20,12 +20,45 @@ namespace DACN
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                if (txtClass.Text.Trim().Equals("") || txtAcademicYear.Text.Trim().Equals("") || txtSubjectId.Text.Trim().Equals("") || txtTerm.Text.Trim().Equals("") || txtLecturerId.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("Required Feilds - Missing Information", "Required Feilds", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    String classid = txtClass.Text;
+                    int lecturerid = Convert.ToInt32(txtLecturerId.Text);
+                    String academyyear = txtAcademicYear.Text;
+                    String subjectid = txtSubjectId.Text;
+                    String term = txtTerm.Text;
+                    String courseid = classid + subjectid;
+                    Boolean insertCourse = course.insertCourse(courseid, subjectid, lecturerid, term, academyyear, classid);
+
+                    if (insertCourse)
+                    {
+                        MessageBox.Show("New Course Has Been Added", "Add Course", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error - Can Not Add New Course", "Add Course", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-           
+            txtTerm.Text = "";
+            txtSubjectId.Text = "";
+            txtLecturerId.Text = "";
+            txtClass.Text = "";
+            txtAcademicYear.Text = "";
         }
     }
 }
