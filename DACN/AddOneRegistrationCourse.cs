3bd4939f8d76b17aa0da133c30058d384712772b
letannam1999 <namlee151199@gmail.com>
2021-04-13 11:23:19 +0700

    Add One Registration Course

--- DACN/AddOneRegistrationCourse.cs
+++ DACN/AddOneRegistrationCourse.cs
@@ -20,12 +20,38 @@ namespace DACN
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                if (txtStudentId.Text.Trim().Equals("") || txtCourseId.Text.Trim().Equals(""))
+                {
+                    MessageBox.Show("Required Feilds - Missing Information", "Required Feilds", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    String courseid = txtCourseId.Text;
+                    int studentid = Convert.ToInt32(txtStudentId.Text);
+                    Boolean insertRegistrationCourse = registrationCourse.insertRegistrationCourse(courseid, studentid);
+
+                    if (insertRegistrationCourse)
+                    {
+                        MessageBox.Show("New Registration Course Has Been Added", "Add Registration Course", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error - Can Not Add New Registration Course", "Add Registration Course", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            
+            txtCourseId.Text = "";
+            txtStudentId.Text = "";
         }
     }
 }
