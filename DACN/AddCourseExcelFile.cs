365febacb52bcd7cfa3693972ad7d2c3c6146dca
letannam1999 <namlee151199@gmail.com>
2021-04-13 09:24:53 +0700

    Add Import Excel File For Course

--- DACN/AddCourseExcelFile.cs
+++ DACN/AddCourseExcelFile.cs
@@ -32,17 +32,76 @@ namespace DACN
 
         private void btnBrowse_Click(object sender, EventArgs e)
         {
-            
+            using (OpenFileDialog openFileDialog = new OpenFileDialog() { Filter = "Excel Sheet (*.xls)|*.xls|WPS Excel (*xlsx)|*.xlsx|All Files(*.*)|*.*" })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtPath.Text = openFileDialog.FileName;
+                    using (var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        using (IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream))
+                        {
+                            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                            });
+                            tableCollection = result.Tables;
+                            cboSheet.Items.Clear();
+                            foreach (DataTable table in tableCollection)
+                                cboSheet.Items.Add(table.TableName);
+                        }
+                    }
+                }
+            }
         }
 
         private void btnImport_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                DapperPlusManager.Entity<ImportCourseExcelFile>().Table("Course");
+                List<ImportCourseExcelFile> excelCourseLists = courseBindingSource.DataSource as List<ImportCourseExcelFile>;
+                if (excelCourseLists != null)
+                {
+                    using (IDbConnection db = connection.GetSqlConnection())
+                    {
+                        db.BulkInsert(excelCourseLists);
+                    }
+                }
+                MessageBox.Show("All information have been imported", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void cboSheet_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            try
+            {
+                DataTable dt = tableCollection[cboSheet.SelectedItem.ToString()];
+                if (dt != null)
+                {
+                    List<ImportCourseExcelFile> imports = new List<ImportCourseExcelFile>();
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        ImportCourseExcelFile import = new ImportCourseExcelFile();
+                        import.SubjectId = dt.Rows[i]["SubjectId"].ToString();
+                        import.LecturerId = dt.Rows[i]["LecturerId"].ToString();
+                        import.Term = dt.Rows[i]["Term"].ToString();
+                        import.AcademicYear = dt.Rows[i]["AcademicYear"].ToString();
+                        import.ClassId = dt.Rows[i]["Classid"].ToString();
+                        import.CourseId = dt.Rows[i]["Classid"].ToString() + dt.Rows[i]["SubjectId"].ToString();
+                        imports.Add(import);
+                    }
+                    courseBindingSource.DataSource = imports;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
